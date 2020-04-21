440802fb9f54833211752c307af752ac78e3307e
Chad Christensen <chadchristensen2@gmail.com>
2020-04-20 20:24:16 -0400

    replace default app component to use board component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as data from '../assets/data/puzzle.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'wheel-of-angular';
+export class AppComponent implements OnInit {
+  puzzles: [] = (data as any).default; 
+  currentPuzzle: string = this.puzzles[Math.floor(Math.random() * this.puzzles.length)];
+
+  ngOnInit() {
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { LetterTileComponent } from './board/letter-tile/letter-tile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    LetterTileComponent
   ],
   imports: [
     BrowserModule,

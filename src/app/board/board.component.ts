4f752ee4b8378c889f789eebc52c4409cdc00ec4
Winnie Zhang <yfz.winnie@gmail.com>
2020-05-06 16:27:52 -0400

    add logic to check if word should be move to next row

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -21,11 +21,22 @@ export class BoardComponent implements OnInit {
 
   createBoard() {
     const puzzleArr = [...this.currentPuzzle];
+    const puzzleWords = this.currentPuzzle.split(" ");
     let puzzleArrIndex = 0;
+    let wordIndex = 0;
+    let moveToNextRow = false;
     this.board = this.boardLayout.map(row => {
-      return row.map(number => {
-        if(puzzleArr[puzzleArrIndex] === ' ') {
+      moveToNextRow = false;
+      return row.map((number, rowIndex) => {
+        if(moveToNextRow) {
+          return "";
+        } else if(puzzleArr[puzzleArrIndex] === ' ') {
           puzzleArrIndex += 1;
+          wordIndex += 1;
+          //to calculate if we should move word to next row
+          if((row.length - rowIndex - 2) < puzzleWords[wordIndex].length) {
+            moveToNextRow = true;
+          }
           return "";
         } else if(number !== 0 && puzzleArrIndex !== puzzleArr.length) {
           const tileObj = { 

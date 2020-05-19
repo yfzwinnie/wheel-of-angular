d5d6401c3b1bbdd66a051e46b37ea6f3f19c6a30
Chad Christensen <chadchristensen2@gmail.com>
2020-05-18 22:03:49 -0400

    handle bankrupt and buying vowel

--- src/app/player-info/player-info.component.ts
+++ src/app/player-info/player-info.component.ts
@@ -1,10 +1,10 @@
 import { PuzzleService } from './../utils/puzzle.service';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-player-info',
   templateUrl: './player-info.component.html',
-  styleUrls: ['./player-info.component.scss']
+  styleUrls: ['./player-info.component.scss'],
 })
 export class PlayerInfoComponent implements OnInit {
   @Input() currentSpinValue: number;
@@ -13,19 +13,29 @@ export class PlayerInfoComponent implements OnInit {
 
   constructor(private puzzleService: PuzzleService) {
     this.puzzleService.letterGuessed$.subscribe((letter) => {
-      if (this.currentPuzzle.toUpperCase().indexOf(letter) > -1) {
-        this.score += this.currentSpinValue * this.calculateLetterOccurences(letter);
+      if ('AEIOU'.indexOf(letter) > -1) {
+        this.score -= 250;
+      } else if (this.currentPuzzle.toUpperCase().indexOf(letter) > -1) {
+        this.score +=
+          this.currentSpinValue * this.calculateLetterOccurences(letter);
       }
     });
-   }
+  }
 
-  calculateLetterOccurences(letter):number {
+  calculateLetterOccurences(letter): number {
     return this.currentPuzzle
-              .split('')
-              .reduce((acc, curr) => curr.toUpperCase() === letter ? acc + 1 : acc , 0);
+      .split('')
+      .reduce(
+        (acc, curr) => (curr.toUpperCase() === letter ? acc + 1 : acc),
+        0
+      );
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.currentSpinValue.currentValue === 0) {
+      this.score = 0;
+    }
+  }
 }
